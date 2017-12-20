80ddc317a59236cb671184cb1a99bc91e89c6099
Shankar <shankar15498@gmail.com>
2017-12-20 21:52:42 +0530

    signup code in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -144,3 +144,17 @@ function signOutCall(){
     console.log("didnt sign out")
   });
 }
+
+function SignUp(){
+
+  email=document.getElementById("email").value;
+  password=document.getElementById("password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+  loginSuccess();
+}
